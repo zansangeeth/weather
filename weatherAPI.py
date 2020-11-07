b721db718e831466b00ced440af1cee325da72fb
zansangeeth <zansangeeth17@gmail.com>
2020-11-07 18:36:57 +0530

    first commit

---
+++ weatherAPI.py
@@ -0,0 +1,38 @@
+import requests, json
+
+api_key = "cc3d56f3ec8bfb8be55ebbef800b6a6a"
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+city_name = input("Enter city name : ")
+
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+
+response = requests.get(complete_url)
+
+x = response.json()
+
+if x["cod"] != "404":
+
+    y = x["main"]
+
+    current_temperature = y["temp"]
+
+    current_pressure = y["pressure"]
+
+    current_humidiy = y["humidity"]
+
+    z = x["weather"]
+    weather_description = z[0]["description"]
+
+    print(" Temperature (in kelvin unit) = " +
+          str(current_temperature) +
+          "\n atmospheric pressure (in hPa unit) = " +
+          str(current_pressure) +
+          "\n humidity (in percentage) = " +
+          str(current_humidiy) +
+          "\n description = " +
+          str(weather_description))
+
+else:
+    print(" City Not Found ")
